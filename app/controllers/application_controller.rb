9d594cfe7f069118f9eb32ed7a5b3931bf496702
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-10 19:21:15 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/posts" do
     Post.create(name: params[:name], content: params[:content])
-    erb :index
+    redirect to '/posts'
   end
 
   get "/posts" do
